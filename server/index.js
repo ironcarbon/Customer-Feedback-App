80785f442c16b4447c58b6867ffee8405efac7c6
ipek <ipekcivicakan@gmail.com>
2019-01-28 19:00:05 -0800

    heroku integration

--- server/index.js
+++ server/index.js
@@ -5,11 +5,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    app.send({ hi: 'ironcarbon' });
+    res.send({ hi: 'ironcarbon' });
 });
 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
-//localhost:5000
+//localhost:5000//localhost:5000//localhost:5000//localhost:5000
