c78c2b311cb84c770d01a3b5f8dfefe568728c64
ipek <ipekcivicakan@gmail.com>
2019-01-29 00:25:16 -0800

    passport setup,enabling google oauth, securing API Keys

--- server/index.js
+++ server/index.js
@@ -1,15 +1,14 @@
 const express = require('express');
-//import express from 'express';
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'ironcarbon' });
-});
+passport.use(new GoogleStrategy());
 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
-//localhost:5000//localhost:5000//localhost:5000//localhost:5000
+//localhost:5000
