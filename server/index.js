c5c2647ce406888bc8d6162a8d2c1523aa20e820
ipek <ipekcivicakan@gmail.com>
2019-02-02 20:09:13 -0800

    Oauth Callbacks

--- server/index.js
+++ server/index.js
@@ -8,14 +8,26 @@ const app = express();
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: 'auth/google/callback'
+    callbackURL: '/auth/google/callback'
 }, accessToken => {
     console.log(accessToken);
 })
 );
 
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+)
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
 //localhost:5000
+
+
+// https://accounts.google.com/o/oauth2/v2/auth?
+// response_type = code & redirect_uri=http % 3A % 2F % 2Flocalhost % 3A5000 % 2Fauth % 2Fauth % 2Fgoogle % 2Fcallback & scope=profile % 20email & client_id=533663942779 - sr13mkuv8vtcum5bb5hli290kq7n4ivl.apps.googleusercontent.com
