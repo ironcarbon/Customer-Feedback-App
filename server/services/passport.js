00657296107590eafa82eb794c98853193d690e9
ipek <ipekcivicakan@gmail.com>
2019-02-05 15:54:12 -0800

    Passport callbacks

--- server/services/passport.js
+++ server/services/passport.js
@@ -13,9 +13,12 @@ passport.use(new GoogleStrategy({
     User.findOne({ googleId: profile.id }).then(existingUser => {
         if (existingUser) {
             //We already have a record with the given profile ID
+            done(null, existingUser);
         } else {
             //we don't have a user record with this ID, make a new record
-            new User({ googleId: profile.id }).save();
+            new User({ googleId: profile.id })
+                .save()
+                .then(user => done(null, user));
         }
     })
 
