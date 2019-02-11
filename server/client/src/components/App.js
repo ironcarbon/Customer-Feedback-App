50157059a0e93495f5d1ba32d44cd61cc0d0ec7e
ipek <ipekcivicakan@gmail.com>
2019-02-11 12:07:09 -0800

    testing FetchUser

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>
@@ -8,6 +10,10 @@ const Landing = () => <h2>Landing</h2>
 
 
 class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+
     render() {
         return (
             <div>
@@ -28,4 +34,4 @@ class App extends Component {
 
 
 
-export default App;
+export default connect(null, actions)(App);
