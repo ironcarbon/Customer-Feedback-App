4b167dfcd937234b8afdbb59cc0a5d7f879f9aad
ipek <ipekcivicakan@gmail.com>
2019-02-12 21:42:11 -0800

    Payments Component

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
 
@@ -10,8 +11,6 @@ class Header extends Component {
                 return;
             case false:
                 return <li><a href='/auth/google'>Login With Google</a></li>
-
-
             default:
                 return <li><a href="/api/logout">Logout</a></li>
 
