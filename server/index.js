785e5d9aa5076c1ea65e15366f370e8edef0bbf0
ipek <ipekcivicakan@gmail.com>
2019-02-14 23:09:21 -0800

    routing in production, adding in a heroku build step

--- server/index.js
+++ server/index.js
@@ -28,16 +28,16 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-    //Express will serve up production assets
-    //like our main.js file, or main.css file!
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
     app.use(express.static('client/build'));
 
-    //Express will serve up the index.html file
-    //if it does not recognize the route
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
     const path = require('path');
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
+    });
     //it means if someone makes a request for a route that express don't understand just serve index.html
 }
 
