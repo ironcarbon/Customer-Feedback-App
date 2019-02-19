4e1c4ab4bb4edb6be0d0d17131252b18bbed1040
ipek <ipekcivicakan@gmail.com>
2019-02-19 08:42:56 -0800

    SurveyNew form added and redux form setup

--- server/client/src/components/Dashboard.js
+++ server/client/src/components/Dashboard.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-const Dashboard = () => {
-    return (
-        <div>
-            Dashboard
+
+class Dashboard extends Component {
+
+    render() {
+
+
+        return (
+            <div>
+                Dashboard
             <div className="fixed-action-btn">
-                <Link to="/surveys/new" className="btn-floating btn-large red">
-                    <i className="material-icons">add</i>
-                </Link>
+                    <Link to="/surveys/new" className="btn-floating btn-large red">
+                        <i className="material-icons">add</i>
+                    </Link>
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
+;
 
 export default Dashboard;
