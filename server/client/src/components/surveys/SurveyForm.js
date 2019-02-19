cf1db1d6d8c37d23177cf686baf1f294f71435d8
ipek <ipekcivicakan@gmail.com>
2019-02-19 11:16:35 -0800

    fields from config

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -1,40 +1,31 @@
 //SurveyForm shows a form for a user to add input
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
 
+const FIELDS = [
+    { label: "Survey Title", name: "title" },
+    { label: "Subject Line", name: "subject" },
+    { label: "Email Body", name: "body" },
+    { label: "Recipient List", name: "email" }
+];
+
 class SurveyForm extends Component {
     renderFields() {
-        return (
-            <div>
+        return _.map(FIELDS, ({ label, name }) => {
+            return (
                 <Field
-                    label="Survey Title"
-                    type="text"
-                    name="title"
+                    key={name}
                     component={SurveyField}
-                />
-                <Field
-                    label="Subject Line"
-                    type="text"
-                    name="subject"
-                    component={SurveyField}
-                />
-                <Field
-                    label="Email Body"
-                    type="text"
-                    name="body"
-                    component={SurveyField}
-                />
-                <Field
-                    label="Recipient List"
-                    type="text"
-                    name="email"
-                    component={SurveyField}
-                />
-            </div>
-        )
+                    type="text" label={label}
+                    name={name} />
+            );
+        });
     }
     // if we add any type of custom prop to the field right here they will be automatically forwarded on to the survey field for us. component="input" was early version
+
+    //lodash map function: use iterate over an array then return a new array consisting of a bunch of different records inside of it.
 
     render() {
         return (
