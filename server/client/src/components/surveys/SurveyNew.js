8010ea4e92d1af79646bda63b4d84fed7da2b2f7
ipek <ipekcivicakan@gmail.com>
2019-02-20 09:07:59 -0800

    SurveyForm to SurveyFormReview with component level state

--- server/client/src/components/surveys/SurveyNew.js
+++ server/client/src/components/surveys/SurveyNew.js
@@ -1,14 +1,31 @@
 // SurveyNew shows SurveyForm and SurveyFormReview
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 
 class SurveyNew extends Component {
+    // constructor(props) {
+    //     super(props);
+
+    //     this.state = { new: true };
+    // }
+
+    state = { showFormReview: false }
+    renderContent() {
+        if (this.state.showFormReview) {
+            return <SurveyFormReview />;
+        }
+
+        return <SurveyForm
+            onSurveySubmit={() => this.setState({ showFormReview: true })}
+        />;
+    }
 
     render() {
         return (
             <div>
-                <SurveyForm />
+                {this.renderContent()}
             </div>
         );
     }
