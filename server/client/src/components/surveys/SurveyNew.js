12c1ccd5bc4370c6edddf50e7b685e372bd3e328
ipek <ipekcivicakan@gmail.com>
2019-02-20 16:17:54 -0800

    Dumping Form Values after clicking the cancel button

--- server/client/src/components/surveys/SurveyNew.js
+++ server/client/src/components/surveys/SurveyNew.js
@@ -1,5 +1,7 @@
 // SurveyNew shows SurveyForm and SurveyFormReview
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import { withRouter } from 'react-router-dom';
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
 
@@ -31,4 +33,6 @@ class SurveyNew extends Component {
     }
 }
 
-export default SurveyNew;
+export default withRouter(reduxForm({
+    form: 'surveyForm'
+})(SurveyNew));
