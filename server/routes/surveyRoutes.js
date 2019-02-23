52ba3ef7cff3afada4e1bf1263285bbf81a51c45
ipek <ipekcivicakan@gmail.com>
2019-02-22 21:44:56 -0800

    Lodash Chain Helper

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -66,19 +66,22 @@ module.exports = app => {
     });
 
     app.post('/api/surveys/webhooks', (req, res) => {
-        const events = _.map(req.body, ({ email, url }) => {
-            const pathname = new URL(url).pathname;
-            const p = new Path('/api/surveys/:surveyId/:choice');
-            const match = p.test(pathname);
-            if (match) {
-                return { email, surveyId: match.surveyId, choice: match.choice }
-            }
-        });
-        //console.log(events)
-        const compactEvents = _.compact(events);
-        const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
 
-        console.log(uniqueEvents);
+        const events = _.chain(req.body)
+            .map(({ email, url }) => {
+                const pathname = new URL(url).pathname;
+                const p = new Path('/api/surveys/:surveyId/:choice');
+                const match = p.test(pathname);
+                if (match) {
+                    return { email, surveyId: match.surveyId, choice: match.choice }
+                }
+            })
+            //console.log(events)
+            .compact()
+            .uniqBy('email', 'surveyId')
+            .value();
+
+        console.log(events);
 
         res.send({});
     });
