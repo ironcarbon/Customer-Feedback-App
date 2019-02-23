01100e7c2cac45fe49fb4cd8519a07c8cf621c5e
ipek <ipekcivicakan@gmail.com>
2019-02-23 13:07:21 -0800

    wiring react to redux

--- server/client/src/components/Dashboard.js
+++ server/client/src/components/Dashboard.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import SurveyList from './surveys/SurveyList';
 
 
-class Dashboard extends Component {
+const Dashboard = () => {
 
-    render() {
-
-
-        return (
-            <div>
-                Dashboard
+    return (
+        <div>
+            <SurveyList />
             <div className="fixed-action-btn">
-                    <Link to="/surveys/new" className="btn-floating btn-large red">
-                        <i className="material-icons">add</i>
-                    </Link>
-                </div>
+                <Link to="/surveys/new" className="btn-floating btn-large red">
+                    <i className="material-icons">add</i>
+                </Link>
             </div>
-        );
-    }
+        </div>
+    );
+
 }
-;
+    ;
 
 export default Dashboard;
