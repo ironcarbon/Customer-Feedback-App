2f3bed468b113878be7173476792d410941655da
ipek <ipekcivicakan@gmail.com>
2019-02-23 17:38:13 -0800

    rendering a list of surveys

--- server/client/src/components/surveys/SurveyList.js
+++ server/client/src/components/surveys/SurveyList.js
@@ -6,17 +6,42 @@ class SurveyList extends Component {
     componentDidMount() {
         this.props.fetchSurveys();
     }
+
+    renderSurveys() {
+        return this.props.surveys.map(survey => {
+            return (
+                <div className="card darken-1" key={survey.id}>
+                    <div className="card-content">
+                        <span className="card-title">{survey.title}</span>
+                        <p>
+                            {survey.body}
+                        </p>
+                        <p className="right">
+                            Sent On:{new Date(survey.dateSent).toLocaleString()}
+                        </p>
+                    </div>
+                    <div className="card-action">
+                        <a>Yes: {survey.yes}</a>
+                        <a>No: {survey.no}</a>
+                    </div>
+                </div>
+            )
+        })
+    }
+
     render() {
         return (
             <div>
-                SurveyList
+                {this.renderSurveys()}
             </div>
         )
     }
 }
 
 function mapStateTopProps(state) {
+
     return { surveys: state.surveys };
+
 }
 
 export default connect(mapStateTopProps, { fetchSurveys })(SurveyList);
